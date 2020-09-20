56a43b9a04a9961e0691b73349cb9e201db77ccd
guangtuan <zhuangguangtuan@gmail.com>
2020-09-20 18:23:17 +0800

    chore: table structure of fixed_expense

--- mysql/init.sql
+++ mysql/init.sql
@@ -83,4 +83,27 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4;
 
+-- ----------------------------
+-- Table structure for fixed_expenses
+-- ----------------------------
+DROP TABLE IF EXISTS `fixed_expenses`;
+CREATE TABLE `fixed_expenses` (
+  `id` int(11) NOT NULL,
+  `form` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'day, week, month, year',
+  `index_of_period` int(11) NOT NULL COMMENT '周期中的第n天',
+  `start` datetime(3) DEFAULT NULL COMMENT '范围起始',
+  `end` datetime(3) DEFAULT NULL COMMENT '范围结束',
+  `generate_type` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '生成方式，real_time 到点生成 all 一次性生成所有',
+  `generated` tinyint(1) DEFAULT NULL COMMENT '1 生成 2 未生成',
+  `amount` int(11) DEFAULT NULL COMMENT '金额：分',
+  `source_account_id` int(11) DEFAULT NULL COMMENT '来源账户id',
+  `dest_account_id` int(11) DEFAULT NULL COMMENT '目标账户id',
+  `user_id` int(11) DEFAULT NULL COMMENT '用户id',
+  `subject_id` int(11) DEFAULT NULL COMMENT '科目id',
+  `remark` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '备注',
+  `fixed_form` int(11) DEFAULT NULL,
+  `index_in_period` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 SET FOREIGN_KEY_CHECKS = 1;
